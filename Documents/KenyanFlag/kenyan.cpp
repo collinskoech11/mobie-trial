3d2c84bfad1fbe9fb23682a69de9b03e52685061
collinskoech11 <collinskoechck34@gmail.com>
2021-02-21 10:07:05 +0300

    update

---
+++ Documents/KenyanFlag/kenyan.cpp
@@ -0,0 +1,37 @@
+using System;
+
+namespace assignment1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("\n" +
+                          "\n" +
+                          "\n" +
+                          "\n" +
+                          "\n" +
+                          "\n" +
+                          "000000000000000000000000000000000000000000000000000000");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("00");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("0000000000000000000000000");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("00");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("000000000000000000000000000000000000000000000000000000\n");
+
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("000000000000000000000000000000000000000000000000000000");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("0000");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write("00000000000000000000");
+            Console.ForegroundColor =ConsoleColor.White;
+            Console.Write("0000");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("0000");
